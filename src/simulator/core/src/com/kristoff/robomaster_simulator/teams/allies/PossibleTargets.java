04ebe935a78d123b51d97078424c056900f6af34
KristoffLiu <zhikangliu@hotmail.com>
2021-06-01 11:03:20 +0800

    update final version 1

---
+++ src/simulator/core/src/com/kristoff/robomaster_simulator/teams/allies/PossibleTargets.java
@@ -0,0 +1,10 @@
+package com.kristoff.robomaster_simulator.teams.allies;
+
+import com.kristoff.robomaster_simulator.utils.Position;
+
+import java.util.List;
+
+public class PossibleTarget {
+    public static List<Position> possibleTargets;
+
+}
